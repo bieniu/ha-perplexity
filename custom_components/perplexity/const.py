22b8fc5fd3e4b6b5f690179863dbc380093dc5f7
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-01-12 16:05:33 +0100

    Remove unused const

--- custom_components/perplexity/const.py
+++ custom_components/perplexity/const.py
@@ -2,9 +2,6 @@
 
 import logging
 
-from homeassistant.const import CONF_LLM_HASS_API
-from homeassistant.helpers import llm
-
 DOMAIN = "perplexity"
 LOGGER = logging.getLogger(__package__)
 
@@ -12,12 +9,6 @@ CONF_PROMPT = "prompt"
 CONF_RECOMMENDED = "recommended"
 
 RECOMMENDED_CHAT_MODEL = "sonar"
-
-RECOMMENDED_CONVERSATION_OPTIONS = {
-    CONF_RECOMMENDED: True,
-    CONF_LLM_HASS_API: [llm.LLM_API_ASSIST],
-    CONF_PROMPT: llm.DEFAULT_INSTRUCTIONS_PROMPT,
-}
 
 PERPLEXITY_MODELS = {
     "sonar": "Sonar",
