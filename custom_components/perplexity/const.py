ac8a773dc4b88139955c0c0fc20e54d6ed1fcd95
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-02-15 13:03:45 +0100

    Support more entity platforms (#79)

--- custom_components/perplexity/const.py
+++ custom_components/perplexity/const.py
@@ -108,14 +108,20 @@ IMPORTANT: You MUST respond with a valid JSON object in this exact format:
 If no action is needed, set "actions" to null or an empty array [].
 
 Common domains and services:
-- light: turn_on (data: brightness, color_temp, rgb_color), turn_off
-- switch: turn_on, turn_off
 - climate: turn_on, turn_off, set_temperature (data: temperature)
 - cover: open_cover, close_cover, set_cover_position (data: position [0-100])
-- media_player: media_play, media_pause, volume_set (data: volume_level [0-1])
-- script: turn_on (to run scripts)
-- scene: turn_on (to activate scenes)
 - fan: turn_on, turn_off, set_percentage (data: percentage [0-100])
+- humidifier: turn_on, turn_off, set_humidity (data: humidity [0-100])
+- light: turn_on (data: brightness [0-255], color_temp, rgb_color), turn_off
+- lock: lock, unlock, open
+- media_player: media_play, media_pause, volume_set (data: volume_level [0-1])
+- scene: turn_on (to activate scenes)
+- script: turn_on (to run scripts)
+- siren: turn_on, turn_off
+- switch: turn_on, turn_off
+- vacuum: start, pause, stop, return_to_base
+- valve: open_valve, close_valve
+- water_heater: turn_on, turn_off, set_temperature (data: temperature)
 
 Always use the entity_id as the target.
 If data is not needed, set it to null.
